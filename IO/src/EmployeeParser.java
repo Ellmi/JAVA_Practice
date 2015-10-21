cbeca5c8ebefc5139ec0aea70f57522465c4b67e
Yanmin <ymxing@thoughtworks.com>
2015-10-21 23:18:10 +0800

    [JAVA_Practice] Yanmin: add data which used in IO module.

---
+++ IO/src/EmployeeParser.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class EmployeeParser {
+    public static void main(String args[]) throws IOException{
+
+        //retrieve all the records into a new array
+        //Scanner scanner = new Scanner(new FileInputStream("Employee.csv"),"UTF-8");
+    }
+}
