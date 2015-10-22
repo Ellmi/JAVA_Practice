fac675f8acf6d5e1490d01d23bb3470631894fe4
Yanmin <ymxing@thoughtworks.com>
2015-10-22 23:44:43 +0800

    [JAVA-Practice] Yanmin: add a new class of Employee and read the file.

---
+++ IO/src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    private String name;
+    private double salary;
+    private int year;
+    private int month;
+    private int day;
+
+
+    public Employee(String name, double salary, int year, int month, int day){
+        this.name = name;
+        this.salary = salary;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
--- IO/src/EmployeeParser.java
+++ IO/src/EmployeeParser.java
@@ -1,9 +1,11 @@
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class EmployeeParser {
     public static void main(String args[]) throws IOException{
 
         //retrieve all the records into a new array
-        //Scanner scanner = new Scanner(new FileInputStream("Employee.csv"),"UTF-8");
+        Scanner scanner = new Scanner(new FileInputStream("/Users/ymxing/non-work/JAVA_Practice/data/Employee.csv"), "UTF-8");
     }
 }
